f4e27583cad3ad5f098b346a175d32ca5b4d58c7
NCarteazy <carter.nicholas.R@gmail.com>
2016-11-22 11:39:23 -0800

    angular/node implementation

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,15 @@ module.exports = function(app) {
 	app.get('*', function(req, res) {
 		res.sendFile('static/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
+
+	app.post('/createuser', function(req, res) {
+		console.log("request received");
+		request.on('data', function(chunk) {
+			body.push(chunk);
+		}).on('end', function(){
+			body = Buffer.concat(body).toString();
+			console.log(body);
+		});
+
+	});
 };
