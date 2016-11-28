dce5a187310d9823ee110754e8f2a4a4edcc47f0
NCarteazy <carter.nicholas.R@gmail.com>
2016-11-28 15:32:49 -0800

    more query support, added bootstrap

--- app/routes.js
+++ app/routes.js
@@ -7,14 +7,14 @@ module.exports = function(app, db) {
 
 	app.post('/createuser', function(req, res) {
 
-		console.log("request received");
-		console.log(req.body.fname + req.body.lname + req.body.uname);
+		console.log("create request received");
 
-		var query = "SELECT firstname, lastname, st_add FROM user WHERE firstname = '" + req.body.fname + "';";
+		var query = "insert into user values ('106','" + req.body.fname + "', '" + req.body.lname + "', '" + req.body.uname + "', '" + req.body.st_add + "', '" + req.body.state + "', '" + req.body.city + "', '" + req.body.zipcode + "', '" + req.body.country + "', '" + req.body.email + "', '" + req.body.mobilenum + "', '" + req.body.homenum + "','" + req.body.payment + "','NULL','NULL','NULL');";
 		console.log("About to run query:" + query);
 
 		db.query(query, function(err, results) {
-			console.log(results);
+			if(err) console.log(err);
+			else console.log(results);
 		});
 	});
 
--- static/js/controllers/auctionCtrl.js
+++ static/js/controllers/auctionCtrl.js
@@ -4,7 +4,13 @@ angular.module('auctionApp', [])
 	.controller('auctionCtrl', ['$scope','$http', function($scope, $http) {
 
 		$scope.regex = {
-			name : /^\s*\w*\s*$/
+			name : /^\s*\w*\s*$/,
+			add : /^\s*\S+(?:\s+\S+){2}$/,
+			zc : /^[0-9]{5}$/,
+			state : /^[A-Z][a-zA-Z]$/,
+			cou : /^[a-zA-Z]{2,}$/,
+			payment : /^[0-9]{16}$/,
+			email : /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
 		};
 
 		$scope.starter = function() {
@@ -15,8 +21,16 @@ angular.module('auctionApp', [])
 			console.log("Button was clicked, " + $scope.user.fname);
 			var data = {'fname':$scope.user.fname,
 						'lname':$scope.user.lname,
-						'uname':$scope.user.uname};
-			console.log()
+						'uname':$scope.user.uname,
+						'st_add':$scope.user.st_add,
+						'state':$scope.user.state,
+						'city':$scope.user.city,
+						'zipcode':$scope.user.zipcode,
+						'country':$scope.user.country,
+						'email':$scope.user.email,
+						'mobilenum':$scope.user.mobilenum,
+						'homenum':$scope.user.homenum,
+						'payment':$scope.user.payment};
 			$scope.status = "loading";
 			$http({
 				method: 'post',
