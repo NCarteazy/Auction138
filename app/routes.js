5222c2b9754570634876c6ee2bdb726e1071255e
NCarteazy <carter.nicholas.r@gmail.com>
2016-11-30 01:25:32 -0800

    tonights works, stable. place bid doesnt fully...
    work and adding list user auctions tomorrow. can add form-inline to
    create user if desired

--- app/routes.js
+++ app/routes.js
@@ -1,64 +1,151 @@
 module.exports = function(app, db) {
 	// application -------------------------------------------------------------
 	app.get('/', function(req, res) {
-		console.log('hi')
+		console.log('hi.')
 		res.sendFile('./static/index.html');
 	});
 
+	//is run after form fufillment
+	//creates a new row in the user table.
 	app.post('/createuser', function(req, res) {
 
 		console.log("createuser");
+		var rq = req.body;
 
-		var query = "insert into user values ('106','" + req.body.fname + "', '" + req.body.lname + "', '" + req.body.uname + "', '" + req.body.st_add + "', '" + req.body.state + "', '" + req.body.city + "', '" + req.body.zipcode + "', '" + req.body.country + "', '" + req.body.email + "', '" + req.body.mobilenum + "', '" + req.body.homenum + "','" + req.body.payment + "','now()','now()','NULL');";
+		//create query by escaping values
+		//query creates a new user row
+		var map = [rq.uid,rq.fname,rq.lname,rq.uname,rq.st_add,rq.state,rq.city,rq.zipcode,rq.country,rq.email,rq.mobilenum,rq.homenum,rq.payment];
+		var query = "insert into user values (?,?,?,?,?,?,?,?,?,?,?,?,?,NULL,NULL,NULL);";
 		console.log("About to run query:" + query);
 
-		db.query(query, function(err, results) {
+		db.query(query, map, function(err, results) {
 			if(err) console.log(err);
-			else console.log(results);
+			else {
+				console.log(results);
+				res.data = results;
+				res.send(results);
+			}
 		});
 	});
 
+	//used as test function
 	app.post('/selectuser', function(req, res) {
 
 		console.log("selectuser");
 		console.log(req.body.fname);
 
+		//no need to escape a single value
+		//query provides some values, used for proof of concept
 		var query = "SELECT firstname, lastname, st_add FROM user WHERE firstname = '" + req.body.fname + "';";
 		console.log("About to run query:" + query);
 
 		db.query(query, function(err, results) {
-			console.log(results);
-			res.data = results;
-			res.send(results);
+			if(err) console.log(err);
+			else {
+				console.log(results);
+				res.data = results;
+				res.send(results);
+			}
 		});
 	});
 
+	//is run on input submission
+	//gets active auctions for given username
 	app.post('/userauctions', function(req, res) {
-		console.log("userauctions");
+		console.log("userauctions, username passed:" + req.body.uname);
 		console.log(req.body);
 
-		var query = "SELECT * FROM auction JOIN auctioneer ON auction.auction_id = aunctioneer.auction_id JOIN user ON auctioneer.user_id = user.user_id  WHERE user_id = ' ';";
+		//todo
+
+		//add input box between header and table
+		//this will take a username and return a small table listing their auctions and statuses.
+
+		var query = "SELECT * FROM auction JOIN auctioneer ON auction.auction_id = auctioneer.auction_id JOIN user ON auctioneer.user_id = user.user_id  WHERE username = '" + req.body.uname + "';";
         console.log("About to run query:" + query);
 
 		db.query(query, function(err, results) {
-			console.log(results);
-			res.data = results;
-			res.send(results);
+			if(err) console.log(err);
+			else {
+				console.log(results);
+				res.data = results;
+				res.send(results);
+			}
 		});
 	});
 
+	//is run on page-load
+	//provides auction information to be displayed
 	app.post('/auctions', function(req, res) {
 		console.log("userauctions");
 		console.log(req.body);
 
-		var query = "SELECT item_name, current_price, buyout_price, end_time, item_condition  FROM auction as a JOIN item as b ON a.item_id = b.item_id;";
+		//this query will always run. it is used to generate the majority of the information used
+		//such as the table on front page and bidding mechanics.
+		var query = "SELECT a.*, item_name, item_condition FROM auction as a JOIN item as b ON a.item_id = b.item_id;";
         console.log("About to run query:" + query);
 
 		db.query(query, function(err, results) {
-			console.log(results);
-			res.data = results;
-			res.send(results);
+			if(err) console.log(err);
+			else {
+				console.log(results);
+				res.data = results;
+				res.send(results);
+			}
 		});
 	});
 
+	app.post('/bid', function(req, res) {
+
+		req.body.start_time = req.body.start_time.replace('Z',' ').replace('T',' ');
+		req.body.end_time = req.body.end_time.replace('Z',' ').replace('T',' ');
+		console.log("bid" + req.body.start_time);
+		var rq = req.body;
+
+
+		//Current status:
+
+		//can't insert new auction into table because it is the same auction,
+		//so the primary key cannot be reused. we would have to delete the
+		//auction and reinsert it for this to work. the true solution is
+		//to change the database to have a new table called bid_on_auction 
+		//where it has foreign keys in both auction and bid
+		//as well as a primary id of its own???? maybe
+		//then auction can be updated with the new 'bid id' and a bid can
+		//be inserted, as well as a column in bid_on_auction
+		//may have to rerun loadauction query
+
+		//first query is updating the auction with the new bid
+		var map = [rq.auction_id,rq.status,rq.auction_type,rq.start_price,rq.reserve_price,rq.buyout_price,rq.curbid,rq.end_price,rq.quantity,rq.start_time,rq.min_bid_incr,rq.end_time,rq.num_of_bids,rq.item_id,rq.bId];
+		var query = "insert into auction values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+		console.log("About to run query:" + query);
+
+		db.query(query, map, function(err, results) {
+			if(err) {
+				console.log("error of bid insert"+err);
+				res.data = results;
+				res.send(results);
+			}
+			else {
+				console.log(results);
+			}
+		});
+		console.log("begin second query");
+		//second query is creating a new row in bid
+		var map = [rq.bId,rq.curbid];
+		var query = "insert into bid values (?, '$', NULL, NULL, 'Active', ?);";
+		console.log("About to run query:" + query);
+
+		db.query(query, map, function(err, results) {
+			if(err) {
+				console.log("error of bid insert"+err);
+				res.data = results;
+				res.send(results);
+			}
+			else {
+				console.log(results);
+				res.data = results;
+				res.send(results);
+			}
+		});
+	});
 };
--- static/js/controllers/auctionCtrl.js
+++ static/js/controllers/auctionCtrl.js
@@ -3,6 +3,11 @@ angular.module('auctionApp', [])
 	// inject the Todo service factory into our controller
 	.controller('auctionCtrl', ['$scope','$http', function($scope, $http) {
 
+		$scope.success = {create:0};
+
+		$scope.uid = Math.floor((Math.random() * 800) + 105);
+
+		$scope.bId = Math.floor((Math.random() * 800) + 6);  
 
 		$scope.user = {
 			list:[
@@ -55,22 +60,24 @@ angular.module('auctionApp', [])
 					text:"Payment Information"
 				},
 			]
-		}
+		};
 
 		$scope.testcreate = function() {
-			console.log("Button was clicked, " + $scope.user.fname);
-			var data = {'fname':$scope.user.fname,
-						'lname':$scope.user.lname,
-						'uname':$scope.user.uname,
-						'st_add':$scope.user.st_add,
-						'state':$scope.user.state,
-						'city':$scope.user.city,
-						'zipcode':$scope.user.zipcode,
-						'country':$scope.user.country,
-						'email':$scope.user.email,
-						'mobilenum':$scope.user.mobilenum,
-						'homenum':$scope.user.homenum,
-						'payment':$scope.user.payment};
+			console.log("Button was clicked, " + $scope.user.list[4].val);
+			var data = {'fname':$scope.user.list[0].val,
+						'lname':$scope.user.list[1].val,
+						'uname':$scope.user.list[2].val,
+						'st_add':$scope.user.list[5].val,
+						'state':$scope.user.list[6].val,
+						'city':$scope.user.list[7].val,
+						'zipcode':$scope.user.list[8].val,
+						'country':$scope.user.list[9].val,
+						'email':$scope.user.list[10].val,
+						'mobilenum':$scope.user.list[3].val,
+						'homenum':$scope.user.list[4].val,
+						'payment':$scope.user.list[11].val,
+						'uid':$scope.uid};
+			$scope.uid = $scope.uid + 1;
 			$scope.status = "loading";
 			$http({
 				method: 'post',
@@ -79,23 +86,24 @@ angular.module('auctionApp', [])
 				}).then(function successCallback(response) {
 					//successfully got a response
 					console.log(response);
-					$scope.answers = response.data;
+					$scope.createduser = response;
+					$scope.success.create = 1;
 					$scope.status = "idle";
 				}, function errorCallback(response) {
 					//usually happens when an exception is thrown
 					console.error(response);
+					$scope.success.create = 0;
 					$scope.status = "failed";
 				});	
 		};
 
 		$scope.testselect = function() {
 			console.log("Button was clicked, " + $scope.user.fname);
-			var data = {'fname':$scope.user.fname};
 			$scope.status = "loading";
 			$http({
 				method: 'post',
 				url: '/selectuser',
-				data: data
+				data: {'fname':$scope.user.fname}
 				}).then(function successCallback(response) {
 					//successfully got a response
 					console.log("res:" + response);
@@ -125,5 +133,33 @@ angular.module('auctionApp', [])
 					$scope.status = "failed";
 				});	
 			};
-		
+
+		$scope.placebid = function(auc) {
+			console.log("Checking if bid is > than min increment and larger than current price");
+			if(auc.min_bid_incr <= (auc.curbid - auc.current_price) && auc.current_price < auc.curbid) {
+				console.log("Confirming bid for auction:" + auc);
+				var conf = confirm("Are you sure you would like to bid $" + auc.curbid + ".00?");
+				if(conf) {
+					console.log("Bid confirmed");
+					auc.num_of_bids = auc.num_of_bids + 1;
+					auc.bId = $scope.bId;
+					$scope.bId = $scope.bId + 1;
+					$scope.status = "loading";
+					$http({
+						method: 'post',
+						url: '/bid',
+						data:auc,
+						}).then(function successCallback(response) {
+							//successfully got a response
+							console.log("res:" + response);
+							$scope.auc = response.data;
+							$scope.status = "idle";
+						}, function errorCallback(response) {
+							//usually happens when an exception is thrown
+							console.error(response);
+							$scope.status = "failed";
+						});	
+				};
+			}
+		};
 	}]);
